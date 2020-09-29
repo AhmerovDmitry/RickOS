88cf27f686356bfbd58b3d00fa55636cc0907b7d
AhmerovDmitry <62261655+AhmerovDmitry@users.noreply.github.com>
2020-09-29 14:01:14 +0300

    Update
    Add mapKit and settings for cerrent location

--- RickApp/SpaceMapController/SpaceMapController.swift
+++ RickApp/SpaceMapController/SpaceMapController.swift
@@ -1,14 +1,57 @@
 import UIKit
+import MapKit
 
 class SpaceMapController: UIViewController {
+    let locationManager = CLLocationManager()
+    var userPinView: MKAnnotationView!
+    let mapView: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        
+        return map
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("SpaceMapController - init")
-        view.backgroundColor = .yellow
+        
+        view.addSubview(mapView)
+        mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        mapView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        mapView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
     deinit {
         print("SpaceMapController - deinit")
+    }
+}
+
+extension SpaceMapController : CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last?.coordinate {
+            let region = MKCoordinateRegion(center: location, latitudinalMeters: 5000, longitudinalMeters: 5000)
+            mapView.setRegion(region, animated: true)
+        }
     }
 }
